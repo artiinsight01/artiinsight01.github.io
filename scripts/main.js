e77f834eb0504131afa0ec46da1ef6a813aa2f06
Lateef Olalekan ADERINOYE <156958596+artiinsight01@users.noreply.github.com>
2024-01-18 12:37:53 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+// Another way of commenting in Javascript
+
+
+/*Using Javascript to perform clicks on images*/
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/dear.jpg") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/dear.jpg");
+  }
+};
+
+
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `I love you so much, ${myName}`;
+  }
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
